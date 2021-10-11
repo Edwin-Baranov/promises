e2f6c2cc549f87659690373d11701e8397fb9308
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-11 16:44:43 -0700

    Completed callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,15 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, text) => {
+    if (text) {
+      var firstLine = text.substr(0, text.indexOf('\n'));
+      callback(null, firstLine);
+    } else {
+      callback(err);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
